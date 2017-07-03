d69c458a08d2155e9e00b8a23326ad56089c1491
Giovanni <giovanni.mancinelli1@studenti.unimi.it>
2017-07-03 17:30:17 +0200

    working on player spline

--- Assets/Scripts/PlayerBezierPath.cs
+++ Assets/Scripts/PlayerBezierPath.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,6 +16,9 @@ public class PlayerBezierPath : MonoBehaviour
     private Vector3[] drawingPoints;
 
     private int n;
+    private PolygonCollider2D coll;
+    private List<Vector2> edgePoints;
+    private bool updateCollider = false;
 
     // Use this for initialization
     void Start()
@@ -23,8 +27,12 @@ public class PlayerBezierPath : MonoBehaviour
         playerCalculatebezier = new CalculateBezierCurve();
         lineRenderer = GetComponent<LineRenderer>();
         playersControlPoints = new List<Vector3>();
+        edgePoints = new List<Vector2>();
         n = 0;
         //gamePoints.Add(GameObject.FindGameObjectWithTag("Start").transform.position);
+        //coll = new GameObject("Collider").AddComponent<PolygonCollider2D>();
+        //coll.transform.parent = lineRenderer.transform;
+        coll = gameObject.AddComponent<PolygonCollider2D>();
     }
 
     // Update is called once per frame
@@ -32,6 +40,7 @@ public class PlayerBezierPath : MonoBehaviour
     {
         hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
         hits = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+        bool rendered = false;
 
         if (Input.GetMouseButtonDown(1))
         {
@@ -85,6 +94,7 @@ public class PlayerBezierPath : MonoBehaviour
                     n++;
                     g.GetComponentInChildren<TextMesh>().text = n.ToString();
                     gamePoints.Add(g);
+                    updateCollider = true;
                 }
             }
             else
@@ -94,12 +104,25 @@ public class PlayerBezierPath : MonoBehaviour
             }
         }
 
-        //if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetMouseButtonUp(0))
+            updateCollider = true;
+
+        if ((gamePoints.Count - 1) % 5 == 0 && gamePoints.Count - 1 != 0)
         {
-            if ((gamePoints.Count - 1) % 5 == 0)
+            Render();
+            if (updateCollider)
             {
-                Render();
+                updateCollider = false;
+                AddCollider();
+                rendered = true;
+                print("x");
             }
+        }
+
+        if(rendered)
+        {
+            Check();
+            rendered = false;
         }
     }
 
@@ -109,6 +132,7 @@ public class PlayerBezierPath : MonoBehaviour
         for (int i = 0; i < gamePoints.Count; i++)
         {
             playersControlPoints.Add(gamePoints[i].transform.position);
+
         }
         playerCalculatebezier.SetControlPoints(playersControlPoints);
         drawingPoints = playerCalculatebezier.GetPlayerDrawingPoints();
@@ -119,5 +143,72 @@ public class PlayerBezierPath : MonoBehaviour
     {
         lineRenderer.positionCount = drawingPoints.Length;
         lineRenderer.SetPositions(drawingPoints);
+    }
+
+    private void AddCollider()
+    {
+        edgePoints = new List<Vector2>();
+        Vector2[] v = new Vector2[drawingPoints.Length];
+        v = drawingPoints.toVector2Array();
+
+        for (int j = 1; j < v.Length; j++)
+        {
+            Vector2 distanceBetweenPoints = v[j - 1] - v[j];
+            Vector3 crossProduct = Vector3.Cross(distanceBetweenPoints, Vector3.forward);
+
+            Vector2 up = (lineRenderer.startWidth / 2) * new Vector2(crossProduct.normalized.x, crossProduct.normalized.y) + v[j - 1];
+            Vector2 down = -(lineRenderer.startWidth / 2) * new Vector2(crossProduct.normalized.x, crossProduct.normalized.y) + v[j - 1];
+
+            edgePoints.Insert(0, down);
+            edgePoints.Add(up);
+
+            if (j == v.Length - 1)
+            {
+                // Compute the values for the last point on the Bezier curve
+                up = (lineRenderer.startWidth / 2) * new Vector2(crossProduct.normalized.x, crossProduct.normalized.y) + v[j];
+                down = -(lineRenderer.startWidth / 2) * new Vector2(crossProduct.normalized.x, crossProduct.normalized.y) + v[j];
+
+                edgePoints.Insert(0, down);
+                edgePoints.Add(up);
+            }
+        }
+
+        coll.points = edgePoints.ToArray();
+        
+    }
+
+    void Check()
+    {
+        if(lineRenderer.positionCount != 0)
+        {
+            RaycastHit2D[] hs = Physics2D.RaycastAll(lineRenderer.GetPosition(0), Vector2.zero);
+
+            //print(hs.Length);
+
+            //for(int i = 0; i < hs.Length; i++)
+            //{
+            //    print(hs[i].collider.name);
+            //}
+            
+            //for(int i = 0; i < lineRenderer.positionCount; i++)
+            //{
+            //    RaycastHit2D[] hitt = Physics2D.RaycastAll(Camera.main.ScreenToWorldPoint(lineRenderer.GetPosition(i)), Vector2.zero);
+            //    RaycastHit2D h = Physics2D.Raycast(lineRenderer.GetPosition(i), Vector2.zero);
+            //    print(hitt.Length);
+            //}
+        }
+    }
+}
+
+public static class MyVector3Extension
+{
+    public static Vector2[] toVector2Array(this Vector3[] v3)
+    {
+        return Array.ConvertAll(v3, getV3fromV2);
+    }
+
+    public static Vector2 getV3fromV2(Vector3 v3)
+    {
+        return new Vector2(v3.x, v3.y);
     }
 }
